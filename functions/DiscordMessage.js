425a94c3d9ee54d1e9eeed1356a900ddc88fedb4
Ben Forster <forsteremailaddress@gmail.com>
2018-12-29 22:27:33 +0000

    Create DiscordMessage.js

---
+++ functions/DiscordMessage.js
@@ -0,0 +1,15 @@
+// const discord = require('discord.js')
+
+
+// module.exports = discord.Structures.extend('Message', Message => {
+//     class DiscordMesssage extends Message {
+//         constructor() {
+//             super()
+//         }
+
+//         send(content) {
+//             this.channel.send(content);
+//         }
+//     }
+//     return DiscordMesssage;
+// });
